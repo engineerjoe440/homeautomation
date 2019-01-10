9ea07511e07995a5c4f48140fc90659991ba6e1c
Joe Stanley <engineerjoe440@yahoo.com>
2019-01-09 17:16:37 -0800

    added allowed hosts line

--- homeautoapp/homeautoapp/settings.py
+++ homeautoapp/homeautoapp/settings.py
@@ -1,3 +1,12 @@
+#########################################################################################
+# Python Django Web Application
+# Used for controlling specific outlets in a home-automation scheme on TS-7500
+# Written by Joe Stanley
+# (c) Stanley Solutions
+#########################################################################################
+
+import os
+from subprocess import call
 from django.conf.urls import url
 from django.http import HttpResponse
 from django.template.loader import render_to_string
@@ -9,6 +18,7 @@ labdir = "C:/Users/Joe Stanley/Desktop/homeautomation/homeautoapp/homeautoapp/La
 
 
 DEBUG = True
+ALLOWED_HOSTS = ['*']
 SECRET_KEY = '$t@nl3y$0lut!0n$w3b@ut0m@t!0n'
 ROOT_URLCONF = __name__
 TEMPLATES = [
@@ -24,23 +34,40 @@ btn1 = file_obj.readline()
 btn2 = file_obj.readline()
 btn3 = file_obj.readline()
 file_obj.close()
-print(btn1,btn2,btn3)
 
-def cont_outputs(output):
+# Send command to OS
+def send_control(cmd):
+	os.system("mkdir new") # Test only
+
+# Determine control needed
+def control(output):
+	if output==1:
+		# Do something for Relay 1
+		send_control(output)
+	elif output ==2:
+		# Do something for Relay 2
+		x = 1
+	elif output ==3:
+		# Do something for Relay 3
+		x = 1
+	else:
+		print("WARNING: Unexpected function call.\nValue outside of range [1,3].")
 	print(output)
 
 def home(request):
 	html = render_to_string('homauto.html', {'B1name': btn1,
 	'B2name': btn2, 'B3name': btn3})
+	warnhtml = render_to_string('warning.html')
 	if(request.GET.get('B1')):
-		cont_outputs(1)
+		control(1)
 	elif(request.GET.get('B2')):
-		cont_outputs(2)
+		control(2)
 	elif(request.GET.get('B3')):
-		cont_outputs(3)
+		control(3)
 	else:
 		if(request.GET.urlencode() != ''):
-			print("Unauthorized or erroneous access attempt!")
+			print("WARNING: Unauthorized or erroneous access attempt!")
+			return(HttpResponse(warnhtml))
 	return HttpResponse(html)
 
 
